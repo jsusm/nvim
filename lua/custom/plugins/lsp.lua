401ce61355bb106fef4cddfbe6feebe033f2d3ce
JsusMarcano <jesusjaviermarcanomora.2005@gmail.com>
2024-04-03 10:46:54 -0400

    add tailwind lsp support for cva

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -101,6 +101,19 @@ local servers = {
   --    https://github.com/pmizio/typescript-tools.nvim
   --
   -- But for many setups, the LSP (`tsserver`) will work just fine
+  tailwindcss = {
+    settings = {
+      tailwindCSS = {
+        experimental = {
+          classRegex = {
+            { 'cva\\(([^)]*)\\)', '["\'`]([^"\'`]*).*?["\'`]' },
+            { 'cx\\(([^)]*)\\)', "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+          },
+        },
+      },
+    },
+  },
+  --
   tsserver = {
     settings = {
       typescript = {
