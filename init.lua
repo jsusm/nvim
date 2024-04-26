d879b0822a386cb2f44b8d86476cc939ad603d19
JsusMarcano <jesusjaviermarcanomora.2005@gmail.com>
2024-04-25 20:14:35 -0400

    remove unused files

--- init.lua
+++ init.lua
@@ -434,9 +434,6 @@ later(function()
   require('dap-go').setup()
 end)
 
-require 'custom.config.options'
 require 'custom.config.keymaps'
-require 'custom.config.autocmds'
 
--- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
